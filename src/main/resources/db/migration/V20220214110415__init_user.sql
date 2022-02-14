c4c08f1fe8a0324c51c2db085e2fc16211536e3f
Jinhao Qi <jinhao.qi@outlook.com>
2022-02-14 11:12:31 +0800

    use flyway and jpa, create the user db

---
+++ src/main/resources/db/migration/V20220214110415__init_user.sql
@@ -0,0 +1,10 @@
+CREATE TABLE t_user (
+                        uid INT AUTO_INCREMENT COMMENT '用户id',
+                        username VARCHAR(20) NOT NULL UNIQUE COMMENT '用户名',
+                        id_number VARCHAR(20) NOT NULL UNIQUE COMMENT '身份证号',
+                        password CHAR(32) NOT NULL COMMENT '密码',
+                        phone VARCHAR(20) COMMENT '电话号码',
+                        gender INT COMMENT '性别:0-女，1-男',
+                        address VARCHAR(50) COMMENT '家庭住址',
+                        PRIMARY KEY (uid)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
