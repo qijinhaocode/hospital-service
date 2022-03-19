5a619ef2b1b64063679a30308d52d23ff2c59fcc
Jinhao Qi <jinhao.qi@outlook.com>
2022-03-19 16:35:09 +0800

    remove validator in ShiftUpdateRequest

--- src/main/java/com/qi/hospital/dto/shift/ShiftUpdateRequest.java
+++ src/main/java/com/qi/hospital/dto/shift/ShiftUpdateRequest.java
@@ -3,38 +3,23 @@ package com.qi.hospital.dto.shift;
 import lombok.Data;
 
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
 
 @Data
 public class ShiftUpdateRequest {
     @NotBlank
     private String doctorJobNumber;
-    @NotNull(message = "预约挂号数不能为空！")
     private Integer weekMondayMorning;
-    @NotNull(message = "预约挂号数不能为空！")
     private Integer weekMondayAfternoon;
-    @NotNull(message = "预约挂号数不能为空！")
     private Integer weekTuesdayMorning;
-    @NotNull(message = "预约挂号数不能为空！")
     private Integer weekTuesdayAfternoon;
-    @NotNull(message = "预约挂号数不能为空！")
     private Integer weekWednesdayMorning;
-    @NotNull(message = "预约挂号数不能为空！")
     private Integer weekWednesdayAfternoon;
-    @NotNull(message = "预约挂号数不能为空！")
     private Integer weekThursdayMorning;
-    @NotNull(message = "预约挂号数不能为空！")
     private Integer weekThursdayAfternoon;
-    @NotNull(message = "预约挂号数不能为空！")
     private Integer weekFridayMorning;
-    @NotNull(message = "预约挂号数不能为空！")
     private Integer weekFridayAfternoon;
-    @NotNull(message = "预约挂号数不能为空！")
     private Integer weekSaturdayMorning;
-    @NotNull(message = "预约挂号数不能为空！")
     private Integer weekSaturdayAfternoon;
-    @NotNull(message = "预约挂号数不能为空！")
     private Integer weekSundayMorning;
-    @NotNull(message = "预约挂号数不能为空！")
     private Integer weekSundayAfternoon;
 }
