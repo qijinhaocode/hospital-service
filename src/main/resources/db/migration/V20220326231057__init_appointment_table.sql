d144c74521b60cf66ad1afaf1576cde72f4f3ce0
Jinhao Qi <jinhao.qi@outlook.com>
2022-04-05 15:27:02 +0800

    modify the length of appointment user id

--- src/main/resources/db/migration/V20220326231057__init_appointment_table.sql
+++ src/main/resources/db/migration/V20220326231057__init_appointment_table.sql
@@ -1,7 +1,7 @@
 CREATE TABLE if NOT EXISTS t_appointment
 (
     uid              VARCHAR(50) COMMENT '预约id',
-    user_id          VARCHAR(20) NOT NULL COMMENT '用户id',
+    user_id          VARCHAR(50) NOT NULL COMMENT '用户id',
     doctor_job_number VARCHAR(20) NOT NULL UNIQUE COMMENT '医生工号',
     localdate        DATE COMMENT '日期',
     appointment_time VARCHAR(20) NOT NULL COMMENT '预约时间',
