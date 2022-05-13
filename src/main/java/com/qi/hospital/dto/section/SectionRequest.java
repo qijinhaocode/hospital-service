b742d31615929f65891f0e6f7e360698d69430b7
Jinhao Qi <jinhao.qi@outlook.com>
2022-05-13 23:32:17 +0800

    [N/A] Refactor code

--- src/main/java/com/qi/hospital/dto/section/SectionRequest.java
+++ src/main/java/com/qi/hospital/dto/section/SectionRequest.java
@@ -1,10 +1,14 @@
 package com.qi.hospital.dto.section;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotBlank;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class SectionRequest {
     @NotBlank
     private String name;
