20ae1efb1dccaf5452f598e9e9c1960bff21f63f
Jinhao Qi <jinhao.qi@outlook.com>
2022-05-24 20:32:32 +0800

    [N/A] Change the db uid length to 36

---
+++ src/main/resources/db/migration/V20220524193021__change_uuid_length.sql
@@ -0,0 +1,16 @@
+ALTER TABLE t_advice MODIFY uid VARCHAR (36);
+ALTER TABLE t_appointment MODIFY uid VARCHAR (36);
+ALTER TABLE t_shift_schedule MODIFY uid VARCHAR (36);
+
+ALTER TABLE t_doctor MODIFY uid VARCHAR (36);
+ALTER TABLE t_shift MODIFY uid VARCHAR (36);
+
+-- 清除外键
+ALTER TABLE t_doctor DROP FOREIGN KEY t_doctor_ibfk_1;
+ALTER TABLE t_section MODIFY uid VARCHAR (36);
+ALTER TABLE t_doctor ADD CONSTRAINT foreign key(section_id) REFERENCES t_section(uid) ON DELETE RESTRICT ON UPDATE RESTRICT;
+
+-- -- 清除外键
+ALTER TABLE t_advice DROP FOREIGN KEY t_advice_ibfk_1;
+ALTER TABLE t_user MODIFY uid VARCHAR (36);
+ALTER TABLE t_advice ADD CONSTRAINT FK_ID foreign key(user_id) REFERENCES t_user(uid) ON DELETE RESTRICT ON UPDATE RESTRICT;
