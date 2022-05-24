782e26b158e7573749704f70232f9995e5ac0e9f
Jinhao Qi <jinhao.qi@outlook.com>
2022-05-24 21:00:58 +0800

    [N/A] Change the db other uid length to 36

--- src/main/resources/db/migration/V20220524193021__change_uuid_length.sql
+++ src/main/resources/db/migration/V20220524193021__change_uuid_length.sql
@@ -1,5 +1,6 @@
 ALTER TABLE t_advice MODIFY uid VARCHAR (36);
 ALTER TABLE t_appointment MODIFY uid VARCHAR (36);
+ALTER TABLE t_appointment MODIFY user_id VARCHAR (36);
 ALTER TABLE t_shift_schedule MODIFY uid VARCHAR (36);
 
 ALTER TABLE t_doctor MODIFY uid VARCHAR (36);
@@ -8,9 +9,12 @@ ALTER TABLE t_shift MODIFY uid VARCHAR (36);
 -- 清除外键
 ALTER TABLE t_doctor DROP FOREIGN KEY t_doctor_ibfk_1;
 ALTER TABLE t_section MODIFY uid VARCHAR (36);
+ALTER TABLE t_doctor MODIFY section_id VARCHAR (36);
 ALTER TABLE t_doctor ADD CONSTRAINT foreign key(section_id) REFERENCES t_section(uid) ON DELETE RESTRICT ON UPDATE RESTRICT;
 
 -- -- 清除外键
 ALTER TABLE t_advice DROP FOREIGN KEY t_advice_ibfk_1;
+ALTER TABLE t_advice MODIFY user_id VARCHAR (36);
 ALTER TABLE t_user MODIFY uid VARCHAR (36);
-ALTER TABLE t_advice ADD CONSTRAINT FK_ID foreign key(user_id) REFERENCES t_user(uid) ON DELETE RESTRICT ON UPDATE RESTRICT;
+ALTER TABLE t_advice ADD CONSTRAINT foreign key(user_id) REFERENCES t_user(uid) ON DELETE RESTRICT ON UPDATE RESTRICT;
+
