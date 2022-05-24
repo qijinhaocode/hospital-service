790193be102bd1d3ac0bde3f6007a182bbc08b53
Jinhao Qi <jinhao.qi@outlook.com>
2022-05-24 21:10:56 +0800

    [N/A] Change the appointment db other uid length to 36

--- src/main/resources/db/migration/V20220524193021__change_uuid_length.sql
+++ src/main/resources/db/migration/V20220524193021__change_uuid_length.sql
@@ -1,6 +1,5 @@
 ALTER TABLE t_advice MODIFY uid VARCHAR (36);
 ALTER TABLE t_appointment MODIFY uid VARCHAR (36);
-ALTER TABLE t_appointment MODIFY user_id VARCHAR (36);
 ALTER TABLE t_shift_schedule MODIFY uid VARCHAR (36);
 
 ALTER TABLE t_doctor MODIFY uid VARCHAR (36);
@@ -17,4 +16,8 @@ ALTER TABLE t_advice DROP FOREIGN KEY t_advice_ibfk_1;
 ALTER TABLE t_advice MODIFY user_id VARCHAR (36);
 ALTER TABLE t_user MODIFY uid VARCHAR (36);
 ALTER TABLE t_advice ADD CONSTRAINT foreign key(user_id) REFERENCES t_user(uid) ON DELETE RESTRICT ON UPDATE RESTRICT;
+
+ALTER TABLE t_appointment DROP FOREIGN KEY t_appointment_ibfk_1;
+ALTER TABLE t_appointment MODIFY user_id VARCHAR (36);
+ALTER TABLE t_appointment ADD CONSTRAINT foreign key(user_id) REFERENCES t_user(uid) ON DELETE RESTRICT ON UPDATE RESTRICT;
 
